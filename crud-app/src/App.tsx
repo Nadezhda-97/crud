13f819778dd7a29d7d24f6364c8b4cfe20c7cb09
nnadya <n.d.niki2012@gmail.com>
2025-03-10 21:18:35 +0300

    update App.tsx

--- crud-app/src/App.tsx
+++ crud-app/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+//import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface News {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const App: React.FC = () => {
+  const [newsList, setNewsList] = useState<News[]>([]);
+  const [currentNews, setCurrentNews] = useState<News | null>(null);
+
+  // Добавление или обновление новости
+  const addOrEditNews = (news: News) => {
+    if (currentNews) {
+      setNewsList(newsList.map((n) => (n.id === news.id ? news : n)));
+      setCurrentNews(null); // сброс текущей новости после обновления
+    } else {
+      setNewsList([...newsList, news]);
+    }
+  };
+
+  // Установка текущей новости для редактирования
+  const editNews = (news: News) => {
+    setCurrentNews(news);
+  };
+
+  // Удаление новости
+  const deleteNews = (id: number) => {
+    setNewsList(newsList.filter((news) => news.id !== id));
+  };
+
+  // Загрузка новостей из локального хранилища при первом рендере
+  useEffect(() => {
+    const storedNews = localStorage.getItem('newsList');
+    if (storedNews) {
+      setNewsList(JSON.parse(storedNews));
+    }
+  }, []);
+
+  // Сохранение новостей в локальное хранилище при изменении списка
+  useEffect(() => {
+    localStorage.setItem('newsList', JSON.stringify(newsList));
+  }, [newsList]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Список новостей</h1>
+      <Form onSubmit={addOrEditNews} currentNews={currentNews}/>
+      <List newsList={newsList} onEdit={editNews} onDelete={deleteNews} />
     </>
   )
 }
 
-export default App
+export default App;
