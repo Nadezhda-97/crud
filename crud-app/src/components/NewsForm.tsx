94c40b1b884a7521bfca295ab1541d2ca29b352f
nnadya <n.d.niki2012@gmail.com>
2025-03-13 18:03:40 +0300

    rename file

---
+++ crud-app/src/components/NewsForm.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import News from "../types/News";
+
+interface FormProps {
+  onSubmit: (news: News) => void;
+}
+
+const NewsForm: React.FC<FormProps> = ({ onSubmit }) => {
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newNews: News = {
+      id: Date.now(),
+      title,
+      description,
+    };
+    onSubmit(newNews);
+    setTitle('');
+    setDescription('');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder="Введите заголовок новости"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          autoFocus
+        />
+        <textarea
+          placeholder="Добавьте описание новости"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <button type="submit">Добавить новость</button>
+      </form>
+    </>
+  )
+};
+
+export default NewsForm;
