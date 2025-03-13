a43c90e4055a9fe642297cbf688b1aaf2ba6d100
nnadya <n.d.niki2012@gmail.com>
2025-03-13 18:04:00 +0300

    rename file

---
+++ crud-app/src/components/NewsList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import News from "../types/News";
+import NewsItem from "./NewsItem";
+
+interface ListProps {
+  newsList: News[];
+  onEdit: (news: News) => void;
+  onDelete: (id: number) => void;
+}
+
+const NewsList: React.FC<ListProps> = ({ newsList, onEdit, onDelete }) => {
+  return (
+    <ul>
+      {newsList.map((news) => (
+        <NewsItem key={news.id} news={news} onEdit={onEdit} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+};
+
+export default NewsList;
